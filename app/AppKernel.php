e304e8faf6354f3e95494ac4e44253655f960049
FlorianLB <flonqueubrochard@clever-age.com>
2013-03-17 23:29:48 +0100

    remove KnpRadBundle from AppKernel (require now manual activation) and add BigBangBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Knp\RadBundle\KnpRadBundle(),
+            new Flug\BigBangBundle\FlubBigBangBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
