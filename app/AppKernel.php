46bbece75584857f595eca95955ee9f16eabcf98
FlorianLB <flonqueubrochard@clever-age.com>
2013-06-01 13:49:42 +0200

    add commented KnpRadBundle config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Flub\BigBangBundle\FlubBigBangBundle(),
+
+            // new Knp\RadBundle\KnpRadBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
