6086c3e055ff11b68cda9ca23aa9e7bec63d393e
FlorianLB <flonqueubrochard@clever-age.com>
2013-06-04 10:21:15 +0200

    few js enhancements

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,17 +18,18 @@ $(document).ready(function(){
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
-  
+
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
+    var position = $($(this).attr("href")).offset().top - 130;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
-    event.preventDefault();    
+    event.preventDefault();
+    location.hash = $(this).attr('href');
   });
-  
+
   sectionHeight();
-  
+
   $('img').load(sectionHeight);
 });
 
